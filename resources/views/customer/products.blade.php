60cd43058b12b40a8f438d6bcb6ca38381637bf6
Aaron Christopher Ramos <aaron.c.s.ramos@gmail.com>
2026-02-01 16:14:35 +0800

    Add page templates, footer, and named routes
    - Introduce customer page templates (aboutus, contacts, products) and extract footer into a reusable partial.
    - Update home template to include navbar/footer and shared CSS via @vite.
    - Add named routes for home, products, contacts, and aboutus in routes/web.php.
    - Also ignore the /.github directory in .gitignore.

---
+++ resources/views/customer/products.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scruffs&Chyrrs - Products</title>
+    @vite(['resources/css/home.css', 'resources/css/page_parts/navbar.css', 'resources/css/page_parts/footer.css'])
+</head>
+
+<body>
+    <div class="upper_body">
+
+        <!-- NAV BAR -->
+        @include('page_parts.navbar')
+
+        <div class="main_content">
+            @yield('content')
+        </div>
+    </div>
+
+    @include('page_parts.footer')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('customer.home');
-});
+})->name('home');
 
 Route::get('/products', function () {
     return view('customer.products');
-});
+})->name('products');
 
 Route::get('/contacts', function () {
     return view('customer.contacts');
-});
+})->name('contacts');
 
 Route::get('/aboutus', function () {
     return view('customer.aboutus');
-});
-
+})->name('aboutus');
